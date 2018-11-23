78fef44a81d57f6e0e66f609b5f5718f37328f46
wangy <yingge_gzb@qq.com>
2018-11-23 17:08:15 +0800

    controller init commit

--- code/EmbryoAI-aCloud/src/main/java/com/embryoai/acloud/rest/RegistrationApi.java
+++ code/EmbryoAI-aCloud/src/main/java/com/embryoai/acloud/rest/RegistrationApi.java
@@ -1,5 +1,14 @@
 package com.embryoai.acloud.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationApi {
+	
+	@PostMapping("/api/v1.0/organization")
+	public Object registOrganization() {
+		return null;
+	}
 
 }
